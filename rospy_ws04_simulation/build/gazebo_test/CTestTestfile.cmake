47226230e5dd0adf59f4433b93e7c3fa7c73b833
stimenaive <997131552@qq.com>
2023-03-29 16:43:43 +0800

    add make file

---
+++ rospy_ws04_simulation/build/gazebo_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gwq/gwq/rospy_ws04_simulation/src/gazebo_test
+# Build directory: /home/gwq/gwq/rospy_ws04_simulation/build/gazebo_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ rospy_ws04_simulation/build/urdf_basic_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gwq/gwq/rospy_ws04_simulation/src/urdf_basic_test
+# Build directory: /home/gwq/gwq/rospy_ws04_simulation/build/urdf_basic_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
